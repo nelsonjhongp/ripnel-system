63811e350740c8d9feab0a9ed8fa0b5a243bcda4
nelsonjhongp <nelsonjhongp@gmail.com>
2025-09-30 15:48:32 -0500

    spring ripnel

---
+++ src/main/java/com/ripnel/system/RipnelSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ripnel.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RipnelSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RipnelSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ripnel/system/RipnelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ripnel.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RipnelSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
